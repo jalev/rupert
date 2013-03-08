67270cae90988d2a93558a799921714eff9c72a2
James Bach <i7983164@bournemouth.ac.uk>
2013-03-08 08:35:22 +0000

    Added size_split and value_from_xml methods
    Added the size_split and value_from_xml methods. I intend to use size_split to
    allow people to pass strings for storage creation. value_from_xml on the other
    hand will parse an xml_dump provided by libvirt and allow us to fetch specific
    values from them.

--- lib/rupert/utility.rb
+++ lib/rupert/utility.rb
@@ -1,7 +1,24 @@
 require 'erb'
+require 'rexml'
 
 module Rupert
   module Utility
+
+    def size_split str
+      #splits a string based on a digit delimiter. 
+      str.split(/(\d+)/)
+    end
+
+    # Return a value from an xml document by navigating a provided tree-path.
+    # We first fetch the xml of an libvirt object by calling its xml_desc
+    # method.
+    #
+    def value_from_xml tree, attribute
+      xml = REXML::Document.new(@xml_desc)
+      # if we didn't provide an attribute, assume that we want a specific
+      # value from a tree, otherwise provide the attributes of an entire tree.
+      attribute.nil? ? xml.elements[tree].text : xml.elements[path].attributes[attribute]
+    end
 
     # Return our template with the values of the template filled in.
     #
